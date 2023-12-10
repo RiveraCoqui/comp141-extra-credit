9e7f39e7090fe1f71d4c46c40cada08b47e78be0
RiveraCoqui <rivera_joanelis@yahoo.com>
2023-12-10 21:35:43 +0000

    changes in directory

---
+++ main.sh
@@ -0,0 +1,28 @@
+mkdir output
+mv data.txt output
+cd output
+touch read.txt
+cat data.txt > read.txt
+touch pwd.txt
+touch ls.txt
+pwd > pwd.txt
+ls > ls.txt
+touch copy.txt
+cp read.txt copy.txt
+touch date.txt
+alias TODAYDATE='date + %d-%m-%y'
+alias TODAYDATE > date.txt
+touch textcount.txt
+wc copy.txt > textcount.txt
+touch process.txt
+ps|head -n 5 > process.txt
+touch netstat.txt
+ifconfig|head -n 5 > netstat.txt
+touch mount.txt
+mount|head -n 5 > mount.txt
+touch permissions.txt
+chmod +rwx permissions.txt
+TESTENV1=test
+touch regex.txt
+grep -vwE '\w{1,3}'copy.txt > regex.txt
+cd -
